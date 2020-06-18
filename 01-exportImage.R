ed92e8286c4b8ef439522d2e1d2100019c1c947d
Cristián <caayala@gmail.com>
2020-06-17 22:38:26 -0400

    recodificaion de variables

--- 01-exportImage.R
+++ 01-exportImage.R
@@ -1,22 +1,26 @@
 library(haven)
-library(tidyverse)
 library(lubridate)
+library(tidyverse)
 library(rdrobust)
-library(foreign)
 
-image <- read_sav("../base_final_anon.sav")
+image <- read_sav("base_final_anon.sav")
 
 #--------------Create cut-off point
 
 #Separate Date from time
-image$Date <- as.Date(image$submitdate)
-image$Time <- format(image$submitdate,"%H:%M:%S")
+image <- image %>% 
+  mutate(Date = as_datetime(submitdate),
+         Time = hms::as_hms(Date))
 
-#Date of the estallido
-image$estallido <- rep("2019-10-19",length(image$id))
+#Date of the estallido and days from survey 
+image <- image %>% 
+  mutate(estallido = as_date("2019-10-19"),
+         date_diff = as_date(Date) - estallido)
 
-image$date_diff <- as.Date(as.character(image$Date), format="%Y-%m-%d") -
-  as.Date(as.character(image$estallido), format="%Y-%m-%d")
+image %>% 
+  select(Date, Time, estallido, date_diff) %>% 
+  slice(c(1:3, (n()-2):n()))
+
 
 #--------------Export
 
@@ -26,7 +30,6 @@ image[image == 88] <- NA
 pride_coutry <- table(image$P1)
 pride_coutry
 
-
 myvars <- c("id", "comuna", "comuna2", "sexo", "edad", 
             "P1", "P2", "P3_3", "P3_7", "P3_8", "P3_9", 
             "P7_1", "P7_2", "P8_2", "P9_2", "P10", 
@@ -34,6 +37,11 @@ myvars <- c("id", "comuna", "comuna2", "sexo", "edad",
             "P13_1", "P13_5", "P13_6", "P14", "P15", "P17", 
             "C2", "C3", "C4", "C5B", "integrantes", "date_diff")
 
-subimage <- image[myvars]
+subimage <- image %>% 
+  select(all_of(myvars))
 
-write.dta(subimage, "../base_analisis.dta")
+write_dta(subimage, 
+          "base_analisis.dta")
+
+saveRDS(subimage, 
+        "base_analisis.rds")
---
+++ 02-pride_protests_analysis.R
@@ -0,0 +1,247 @@
+#********************************************************************************
+#  Pride & Protest 
+#  Francisco Olivos
+#  June 11
+#  Balance and main models
+#********************************************************************************
+
+#Still to do and consider:
+#            - Tenemos que preparar varias cosas en el material suplementario. 
+#              pero eso lo podemos hacer despues.
+#            - Probare algunos analisis por submuestra. Principalmente hay un 
+#              perfil de jovenes, hombres y mas educados que se asocia con protesta.
+#              quiero ver si hay diferencias significativas entre esos grupos que podamos destacar. 
+#            - Incluir el grafico de las encuestas por dia es un poco riesgoso. Porque luego de la segunda tanda
+#              de encuesta, se volvio a parar una semana. Y si mostramos eso nos van a cuestionar inmediatamente. 
+
+
+library(tidyverse)
+
+pride <- readRDS("base_analisis.rds")
+
+# DATA MANAGEMENT ---------------------------------------------------------
+
+rename_label <- function(df, ..., label){
+  vars <- enquos(...)
+  
+  df <- rename(df, !!!enquos(...))
+  attr(df[[names(vars)[1]]], 'label') <- label
+  
+  return(df)
+}
+
+
+# Estallido social: post treatement.
+pride$treat <- if_else(pride$date_diff > 0, TRUE, FALSE)
+attr(pride$treat, 'label') <- 'Social crisis'
+
+pride %>% count(treat)
+
+# Country Pride
+pride <- pride %>% 
+  rename_label(pride_CL = P1, label = "Pride toward the country")
+
+# Energy
+pride <- pride %>% 
+  rename_label(energy = P3_3, label = "Proud of Chileans: Energy")
+
+# Symbols
+pride <- pride %>% 
+  rename_label(pride_sym = P3_9, label = "Proud of Chile: Symbols")
+
+# Economic Development
+pride <- pride %>% 
+  rename_label(pride_dev = P11_1, label = "Proud of Chile: Economic development")
+
+# Place to Live
+pride <- pride %>% 
+  rename_label(pride_pl = P11_3, label = "Proud of Chile: Place to live")
+
+# Effort
+pride <- pride %>% 
+  mutate(pride_esf = case_when(P14 == 2 ~ 1L,
+                               P14 %in% c(1, 3:6) ~ 0L,
+                               TRUE ~ NA_integer_))
+attr(pride$pride_esf, 'label') <- 'Proud of Chileans: Effort'
+
+count(pride, P14, pride_esf)
+
+
+pride <- pride %>% 
+  mutate(across(c(treat, pride_CL, energy, 
+                pride_esf, pride_sym, pride_dev, pride_pl),
+                as_factor))
+
+
+pride$treat<-as.factor(treat) 
+pride$pride_CL<-as.factor(pride_CL) 
+pride$energy <-as.factor(energy)
+pride$pride_esf<-as.factor(pride_esf) 
+pride$pride_sym <-as.factor(pride_sym)
+pride$pride_dev <-as.factor(pride_dev)
+pride$pride_pl<-as.factor(pride_pl)
+
+prideLW <- pride[ which(sample==1),] #Listwise
+detach(pride)
+attach(prideLW)
+
+summary(prideLW)
+
+#:::::::::::::::::::::::::Balance
+
+#La idea es comparar las medias y distribuciones de las variables. Pueden ser
+#boxplots o la media con el intervalo de confianza. Cualquiera visualizacion
+#que permita mostrar el balance va a estar bien. Hay unas ligeras diferencias
+#por zonas geograficas, pero con la correcion del ebal quedan bien. 
+
+#Abajo genere unos graficos con la media de cada una de las variables. Ojo con
+#que uso la base de datos prideLW. Es un subset que considera solo casos sin 
+#missing. La variable "sample" viene de la base de datos en Stata. 
+
+#Las variables son casi todas 0 a 1. Son las variables categoricas transformadas
+#en binarias. Reportar la media de eso esta perfect. 
+
+
+library(ggpubr)
+library(dplyr) 
+# Create new dataframe mpg_means_se
+
+genderplot<-ggplot(prideLW, aes(x=treat, y=gender)) + geom_bar(stat="summary") + 
+  ylim(0, 1) +labs(title=" ", x =" ", y = "Proportion") +
+  geom_errorbar(stat="summary")
+
+zone1plot<-ggplot(prideLW, aes(x=treat, y=zone1)) + geom_bar(stat="summary") + 
+  ylim(0, 1) +labs(title=" ", x =" ", y = "Proportion") +
+  geom_errorbar(stat="summary")
+
+zone2plot<-ggplot(prideLW, aes(x=treat, y=zone2)) + geom_bar(stat="summary") + 
+  ylim(0, 1) +labs(title=" ", x =" ", y = "Proportion") +
+  geom_errorbar(stat="summary")
+
+zone3plot<-ggplot(prideLW, aes(x=treat, y=zone3)) + geom_bar(stat="summary") + 
+  ylim(0, 1) +labs(title=" ", x =" ", y = "Proportion") +
+  geom_errorbar(stat="summary")
+
+zone4plot<-ggplot(prideLW, aes(x=treat, y=zone4)) + geom_bar(stat="summary") + 
+  ylim(0, 1) +labs(title=" ", x =" ", y = "Proportion") +
+  geom_errorbar(stat="summary")
+
+edu1plot<-ggplot(prideLW, aes(x=treat, y=edu1)) + geom_bar(stat="summary") + 
+  ylim(0, 1) +labs(title=" ", x =" ", y = "Proportion") +
+  geom_errorbar(stat="summary")
+
+edu2plot<-ggplot(prideLW, aes(x=treat, y=edu2)) + geom_bar(stat="summary") + 
+  ylim(0, 1) +labs(title=" ", x =" ", y = "Proportion") +
+  geom_errorbar(stat="summary")
+
+edu3plot<-ggplot(prideLW, aes(x=treat, y=edu3)) + geom_bar(stat="summary") + 
+  ylim(0, 1) +labs(title=" ", x =" ", y = "Proportion") +
+  geom_errorbar(stat="summary")
+
+household1plot<-ggplot(prideLW, aes(x=treat, y=household1)) + geom_bar(stat="summary") + 
+  ylim(0, 1) +labs(title=" ", x =" ", y = "Proportion") +
+  geom_errorbar(stat="summary")
+
+household2plot<-ggplot(prideLW, aes(x=treat, y=household2)) + geom_bar(stat="summary") + 
+  ylim(0, 1) +labs(title=" ", x =" ", y = "Proportion") +
+  geom_errorbar(stat="summary")
+
+household3plot<-ggplot(prideLW, aes(x=treat, y=household3)) + geom_bar(stat="summary") + 
+  ylim(0, 1) +labs(title=" ", x =" ", y = "Proportion") +
+  geom_errorbar(stat="summary")
+
+household4plot<-ggplot(prideLW, aes(x=treat, y=household4)) + geom_bar(stat="summary") + 
+  ylim(0, 1) +labs(title=" ", x =" ", y = "Proportion") +
+  geom_errorbar(stat="summary")
+
+ggarrange(genderplot, zone1plot, zone2plot, zone3plot, zone4plot,
+          edu1plot, edu2plot, edu3plot, household1plot, household2plot, household3plot, household4plot  +  rremove("x.text"), 
+          labels = c("Female", "North", "Center", "Mat. Region", "South", "Low edu.", "Middle edu.", "High edu.", "One", "Two", "Three", "Four+"),
+          font.label = list(size = 10),
+          ncol = 3, nrow = 4)
+
+#:::::::::::::::::::::::::REGRESIONS
+
+library("arm")
+
+#::::::Effects on moral sentiments toward the country 
+
+#Estime los modelos para cada una de las variables. Trato de replicar los analisis
+#que ya incluimos en la propuesta. Solo tengo problemas con los modelos C que no
+#me permiten incluir el weight. No se la razon.
+
+#El entropy balance lo hice en Stata. El weight es webal. 
+
+#Pride toward the country
+
+m1A <- lm(formula = pride_CL ~ treat, data = prideLW)
+m1B <- lm(formula = pride_CL ~ treat + gender + age + zone1 + zone2 + zone4 +
+          edu1 + edu3 + household2 + household3 + household4, data = prideLW)
+m1C <- lm(formula = pride_CL ~ treat, data = prideLW) #error when adding weight=webal
+
+par(mfrow = c(1, 1))
+coefplot(m1A, xlim=c(-.6, 0), main = "Country Pride", intercept=FALSE)
+coefplot(m1B, add=TRUE, col.pts="red",  intercept=FALSE)
+coefplot(m1C, add=TRUE, col.pts="blue", intercept=FALSE, offset=0.2)
+
+#Pride economic development
+m2A <- lm(formula = pride_dev ~ treat, data = prideLW)
+m2B <- lm(formula = pride_dev ~ treat + gender + age + zone1 + zone2 + zone4 +
+            edu1 + edu3 + household2 + household3 + household4, data = prideLW)
+m2C <- lm(formula = pride_dev ~ treat, data = prideLW) #error when adding weight=webal
+
+par(mfrow = c(1, 1))
+coefplot(m2A, xlim=c(-.6, 0), main = "Economic Development", intercept=FALSE)
+coefplot(m2B, add=TRUE, col.pts="red",  intercept=FALSE)
+coefplot(m2C, add=TRUE, col.pts="blue", intercept=FALSE, offset=0.2)
+
+#Pride symbols
+m3A <- lm(formula = pride_sym ~ treat, data = prideLW)
+m3B <- lm(formula = pride_sym ~ treat + gender + age + zone1 + zone2 + zone4 +
+            edu1 + edu3 + household2 + household3 + household4, data = prideLW)
+m3C <- lm(formula = pride_sym ~ treat, data = prideLW) #error when adding weight=webal
+
+par(mfrow = c(1, 1))
+coefplot(m3A, xlim=c(-.6, 0), main = "Symbols", intercept=FALSE)
+coefplot(m3B, add=TRUE, col.pts="red",  intercept=FALSE)
+coefplot(m3C, add=TRUE, col.pts="blue", intercept=FALSE, offset=0.2)
+
+#Place to live
+m4A <- lm(formula = pride_pl ~ treat, data = prideLW)
+m4B <- lm(formula = pride_pl ~ treat + gender + age + zone1 + zone2 + zone4 +
+            edu1 + edu3 + household2 + household3 + household4, data = prideLW)
+m4C <- lm(formula = pride_pl ~ treat, data = prideLW) #error when adding weight=webal
+
+par(mfrow = c(1, 1))
+coefplot(m4A, xlim=c(-.6, 0), main = "Place", intercept=FALSE)
+coefplot(m4B, add=TRUE, col.pts="red",  intercept=FALSE)
+coefplot(m4C, add=TRUE, col.pts="blue", intercept=FALSE, offset=0.2)
+
+#::::::Effects on moral sentiments toward Chileans
+
+#Energy
+
+m5A <- lm(formula = energy ~ treat, data = prideLW)
+m5B <- lm(formula = energy ~ treat + gender + age + zone1 + zone2 + zone4 +
+            edu1 + edu3 + household2 + household3 + household4, data = prideLW)
+m5C <- lm(formula = energy ~ treat, data = prideLW) #error when adding weight=webal
+
+par(mfrow = c(1, 1))
+coefplot(m5A, xlim=c(0, .5), main = "Energy", intercept=FALSE)
+coefplot(m5B, add=TRUE, col.pts="red",  intercept=FALSE)
+coefplot(m5C, add=TRUE, col.pts="blue", intercept=FALSE, offset=0.2)
+
+#Effort
+
+m6A <- lm(formula = pride_esf ~ treat, data = prideLW)
+m6B <- lm(formula = pride_esf ~ treat + gender + age + zone1 + zone2 + zone4 +
+            edu1 + edu3 + household2 + household3 + household4, data = prideLW)
+m6C <- lm(formula = pride_esf ~ treat, data = prideLW) #error when adding weight=webal
+
+par(mfrow = c(1, 1))
+coefplot(m6A, xlim=c(0, .5), main = "Effort", intercept=FALSE)
+coefplot(m6B, add=TRUE, col.pts="red",  intercept=FALSE)
+coefplot(m6C, add=TRUE, col.pts="blue", intercept=FALSE, offset=0.2)
+
+
+

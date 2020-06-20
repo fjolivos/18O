1184ef29f0d979f081a4dd1aa1f79c9a755886a3
Cristián <caayala@gmail.com>
2020-06-20 12:38:52 -0400

    add reference to master database
    Database outside of repository to build one only with variables of interest.

--- 01-exportImage.R
+++ 01-exportImage.R
@@ -1,14 +1,14 @@
 library(haven)
 library(lubridate)
 library(tidyverse)
-library(rdrobust)
 
-image <- read_dta("base_analisis.dta")
+# Database outside of repository
+image <- read_sav("../base_final_anon.sav")
 
 region <- read_dta('pride_protests.dta')
 
 image <- image %>% 
-  left_join(region %>% select(id, geozone),
+  left_join(region %>% select(id, webal, geozone),
             by = 'id')
 
 #--------------Create cut-off point
@@ -30,12 +30,13 @@ image %>%
 
 # Export
 
-myvars <- c("id", "comuna", "comuna2", "geozone", "sexo", "edad", 
+myvars <- c("id", "webal", "comuna", "comuna2", "geozone", "sexo", "edad", 
             "P1", "P2", "P3_3", "P3_7", "P3_8", "P3_9", 
             "P7_1", "P7_2", "P8_2", "P9_2", "P10", 
             "P11_1", "P11_3", "P11_5", "P11_6", "P12_3", "P12_4", 
             "P13_1", "P13_5", "P13_6", "P14", "P15", "P17", 
-            "C2", "C3", "C4", "C5B", "integrantes", "date_diff")
+            "C2", "C3", "C4", "C5B", 
+            "integrantes", "Date", "Time", "date_diff")
 
 subimage <- image %>% 
   select(all_of(myvars))
